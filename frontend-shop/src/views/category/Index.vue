32107c3c428f0b3debde24d3406b769b2921e384
muhislah <el.islah.udin@gmail.com>
2023-09-03 06:22:45 +0700

    change image size

--- frontend-shop/src/views/category/Index.vue
+++ frontend-shop/src/views/category/Index.vue
@@ -2,11 +2,11 @@
     <div class="container-fluid mb-5 mt-4">
   
           <div class="row">
-              <div v-for="category in categories" :key="category.id" class="col-md-2 col-4 mb-3">
+              <div v-for="category in categories" :key="category.id" class="col-md-2 col-6 mb-3">
                   <router-link :to="{name: 'detail_category', params:{slug: category.slug}}">
                       <div class="card h-100 border-0 rounded shadow">
                           <div class="card-body text-center">
-                              <img :src="category.image" style="width: 100px;">
+                              <img :src="category.image" style="width: 100px; object-fit: contain; ">
                               <hr>
                               <label class="font-weight-bold">{{ category.name }}</label>
                           </div>
